b9c1d334dd9d666929a9889f1e85a27ae9d93010
shrutigrover <shrutigrover693@gmail.com>
2020-02-24 23:48:47 -0500

    added component communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
+  featureSelected = 'about';
+
+  onSelectFeature(feature : string){
+    this.featureSelected = feature;
+  }
 }
--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,10 +9,16 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes : Recipe[] = [new Recipe("Test", "This is a test recipe", "https://i2.wp.com/flavouroma.com/wp-content/uploads/2018/11/restaurant-style-rajma-recipe.jpg?resize=840%2C560&ssl=1"),new Recipe("Test", "This is a test recipe", "https://i2.wp.com/flavouroma.com/wp-content/uploads/2018/11/restaurant-style-rajma-recipe.jpg?resize=840%2C560&ssl=1")];
+  @Output() recipeSelectedEvent = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  recipeSelected(recipe : Recipe){
+    console.log("recipe list");
+    this.recipeSelectedEvent.emit(recipe);
+  }
+  
 }
